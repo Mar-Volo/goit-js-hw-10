fd019225feb661490c88e88b4a693d516ae779be
Volodymyr Marchenko <Katalizator1992@gmail.com>
2023-01-20 14:28:45 +0200

    finish-hw-10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,17 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
-function fetchCountries(name) {
-
+import Notiflix from 'notiflix';
+function fetchCountries(name = 'Ukraine') {
+  return fetch(
+    `${BASE_URL}${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(resp => {
+      if (!resp.ok) {
+        throw new Error(
+          Notiflix.Notify.failure('Oops, there is no country with that name')
+        );
+      }
+      return resp.json();
+    })
+    .catch(err => console.error(err));
 }
-export default { fetchCountries };
+export default fetchCountries;
--- src/index.js
+++ src/index.js
@@ -8,8 +8,59 @@ const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
 searchBox.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+countryInfo.style.display = 'none';
 
 function searchCountry(e) {
-
+  const mainCountry = e.target.value.trim();
+  if (mainCountry === '') {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+    countryInfo.style.display = 'none';
+    return;
+  }
+  fetchCountries(mainCountry).then(data => {
+    if (data.length >= 2 && data.length <= 10) {
+      countryList.innerHTML = '';
+      countryInfo.innerHTML = '';
+      countryInfo.style.display = 'none';
+      createCountryList(data);
+    } else if (data.length === 1) {
+      countryList.innerHTML = '';
+      countryInfo.innerHTML = '';
+      countryInfo.style.display = 'flex';
+      createCountryCard(data);
+    } else if (data.length > 10) {
+      Notiflix.Notify.info(
+        'Too many matches found. Please enter a more specific name.'
+      );
+    }
+  });
 }
-
+function createCountryList(arr) {
+  const list = arr
+    .map(({ flags, name }) => {
+      return `<li class="country__item">
+  <img src="${flags.svg}" alt="${name.official}" width="30" height="30" />
+  <h2 class="country__name">${name.common}</h2>
+</li>`;
+    })
+    .join('');
+  countryList.insertAdjacentHTML('beforeend', list);
+}
+function createCountryCard(arr) {
+  const card = arr
+    .map(({ flags, name, capital, population, languages }) => {
+      return `<div class="country__title">
+  <img src="${flags.svg}" alt="${name.official}" width="55" height="45" />
+  <h1 class="country__name">${name.official}</h1>
+</div>
+<ul>
+  <li class="country__capital item">${capital}</li>
+  <li class="country__population item">${population}</li>
+  <li class="country__languages item">${Object.values(languages)}</li>
+</ul>`;
+    })
+    .join('');
+  countryInfo.insertAdjacentHTML('beforeend', card);
+  console.log(arr);
+}
