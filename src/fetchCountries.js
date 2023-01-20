18f5f8ab2c169e67c81427f4af9824dc4833a67b
Volodymyr Marchenko <Katalizator1992@gmail.com>
2023-01-20 11:33:46 +0200

    add-constants-and-imports

---
+++ src/fetchCountries.js
@@ -0,0 +1,5 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+function fetchCountries(name) {
+
+}
+export default { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import fetchCountries from './fetchCountries';
 
+const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
+searchBox.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry(e) {
+
+}
+
