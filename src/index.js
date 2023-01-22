9efa136a45bd0c4bb73aa548a40c962a9f74b54b
Volodymyr Marchenko <Katalizator1992@gmail.com>
2023-01-22 22:25:16 +0200

    fix-input

--- src/index.js
+++ src/index.js
@@ -12,36 +12,31 @@ countryInfo.style.display = 'none';
 
 function searchCountry(e) {
   const mainCountry = e.target.value.trim();
-  if (mainCountry === '') {
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
-    countryInfo.style.display = 'none';
-    return;
+  countryInfo.style.display = 'none';
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+  if (mainCountry !== '') {
+    fetchCountries(mainCountry)
+      .then(data => {
+        if (data.length >= 2 && data.length <= 10) {
+          // countryInfo.style.display = 'none';
+          createCountryList(data);
+        } else if (data.length === 1) {
+          countryInfo.style.display = 'flex';
+          createCountryCard(data);
+        } else if (data.length > 10) {
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        }
+      })
+      .catch(
+        err => (
+          console.error(err.toString()),
+          Notiflix.Notify.failure('Oops, there is no country with that name')
+        )
+      );
   }
-  fetchCountries(mainCountry)
-    .then(data => {
-      if (data.length >= 2 && data.length <= 10) {
-        countryList.innerHTML = '';
-        countryInfo.innerHTML = '';
-        countryInfo.style.display = 'none';
-        createCountryList(data);
-      } else if (data.length === 1) {
-        countryList.innerHTML = '';
-        countryInfo.innerHTML = '';
-        countryInfo.style.display = 'flex';
-        createCountryCard(data);
-      } else if (data.length > 10) {
-        Notiflix.Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-      }
-    })
-    .catch(
-      err => (
-        console.error(err.toString()),
-        Notiflix.Notify.failure('Oops, there is no country with that name')
-      )
-    );
 }
 function createCountryList(arr) {
   const list = arr
